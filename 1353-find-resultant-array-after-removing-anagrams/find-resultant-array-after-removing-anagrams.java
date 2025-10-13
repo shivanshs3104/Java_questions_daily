8e66fbd9c001832c6efa0147c22c40e44fcbe93f
Shivansh Singh <142593288+shivanshs3104@users.noreply.github.com>
2025-10-13 09:50:45 +0530

    Time: 9 ms (10.12%) | Memory: 45 MB (33.97%) - LeetSync

--- 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.java
@@ -1,21 +1,20 @@
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        List<String> res = new ArrayList<>();
-        res.add(words[0]);
-        for (int i = 1; i < words.length; i++) {
-            if (!isAnagram(words[i], res.get(res.size() - 1))) {
-                res.add(words[i]);
-            }
+        int n = words.length;
+        List<Map<Character, Integer>> freq = new ArrayList<>();
+        for (String w : words) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : w.toCharArray())
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            freq.add(map);
         }
-        return res;
-    }
 
-    private boolean isAnagram(String a, String b) {
-        if (a.length() != b.length()) return false;
-        int[] count = new int[26];
-        for (char c : a.toCharArray()) count[c - 'a']++;
-        for (char c : b.toCharArray()) count[c - 'a']--;
-        for (int x : count) if (x != 0) return false;
-        return true;
+        List<String> ans = new ArrayList<>();
+        ans.add(words[0]);
+        for (int i = 1; i < n; i++) {
+            if (!freq.get(i).equals(freq.get(i - 1)))
+                ans.add(words[i]);
+        }
+        return ans;
     }
 }
